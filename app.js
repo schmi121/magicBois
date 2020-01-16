52179b5351ceafdb01da59a8473134b08c563059
Jordan Schmidt <57489956+schmi121@users.noreply.github.com>
2020-01-15 17:52:10 -0800

    fb 1.0.1

--- app.js
+++ app.js
@@ -15,126 +15,72 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var magicPlayers = firebase.database().ref("/magic/players");
 
-    magicPlayers.set({
-        Jordan: {
-            score: 0,
-        },
+    let usersChoice;
 
-        Dillon: {
-            score: 0,
-        },
+    const jordanButton = document.getElementById('jordan');
+    const blakeButton = document.getElementById('blake');
+    const toneButton = document.getElementById('tone');
+    const dillonButton = document.getElementById('dillon');
+    const willButton = document.getElementById('will');
 
-        Tone: {
-            score: 0,
-        },
+    jordanButton.addEventListener('click', () => {
+        usersChoice = 'jordan';
+        playGame();
+    });
 
-        Blake: {
-            score: 0,
-        },
+    blakeButton.addEventListener('click', () => {
+        usersChoice = 'blake';
+        playGame();
+    });
 
-        Will: {
-            score: 0,
-        },
+    toneButton.addEventListener('click', () => {
+        usersChoice = 'tone';
+        playGame();
+    });
+
+    dillonButton.addEventListener('click', () => {
+        usersChoice = 'dillon';
+        playGame();
+    });
+    willButton.addEventListener('click', () => {
+        usersChoice = 'will';
+        playGame();
     });
 
     var jordanScoreRef = firebase.database().ref("magic/players/Jordan").child('score');
 
-    jordanScoreRef.transaction(function(currentScore) ({
+    jordanScoreRef.transaction(function (currentScore) ({
         return currentScore + 1;
     });
 
-    // const preObject = document.getElementById('object');
+    var magicPlayersRef = db.collection('players')
 
-    // const dbRefObject = firebase.database().ref().child('object');
-
-    // dbRefObject.on('value', snap => console.log(snap.val()));
-
-
-
-
-    // const magicBois = ['jordan', 'tone', 'blake', 'dillon', 'will'];
-
-    // let cpuChoice = magicBois[Math.floor(Math.random() * magicBois.length)];
-    // console.log(cpuChoice)
-
-    // let points;
-
-    // let usersChoice;
-
-    // const scoreBoard = {
-    //     playerScore: 0,
-    //     cpuScore: 0,
-    //     incrementPlayerScore: function() {
-    //         this.playerScore += 1;
-    //     },
-    //     incrementCpuScore: function() {
-    //         this.cpuScore += 1;
-    //     },
-    //     winnerCheck: function() {
-    //         if (this.playerScore === 5) {
-    //             alert("You Win!");
-    //             this.resetGame();
-    //         } else if (this.cpuScore === 5) {
-    //             alert("You Lose");
-    //             this.resetGame();
-    //         }
-    //     },
-    //     resetGame: function() {
-    //         this.playerScore = 0;
-    //         this.cpuScore = 0;
-    //         alert('The game will now reset');
-    //     },
-    // };
-
-    // const jordanButton = document.getElementById('jordan');
-    // const blakeButton = document.getElementById('blake');
-    // const toneButton = document.getElementById('tone');
-    // const dillonButton = document.getElementById('dillon');
-    // const willButton = document.getElementById('will');
-
-    // jordanButton.addEventListener('click', () => {
-    //     usersChoice = 'jordan';
-    //     playGame();
-    // });
-
-    // blakeButton.addEventListener('click', () => {
-    //     usersChoice = 'blake';
-    //     playGame();
-    // });
-
-    // toneButton.addEventListener('click', () => {
-    //     usersChoice = 'tone';
-    //     playGame();
-    // });
-
-    // dillonButton.addEventListener('click', () => {
-    //     usersChoice = 'dillon';
-    //     playGame();
-    // });
-    // willButton.addEventListener('click', () => {
-    //     usersChoice = 'will';
-    //     playGame();
-    // });
-
-    // function playGame() {
-    //     if (usersChoice === 'rock' && cpuChoice === 'scissors') {
-    //         alert('You won');
-    //         scoreBoard.playerScore++
-    //     } else if (usersChoice === 'paper' && cpuChoice === 'rock') {
-    //         alert('You won');
-    //         scoreBoard.playerScore++
-    //     } else if (usersChoice === 'scissors' && cpuChoice === 'paper') {
-    //         alert('You won'); 
-    //         scoreBoard.playerScore++
-    //     } else if (usersChoice === cpuChoice) {
-    //         alert('You Tied');
-    //     } else {
-    //         alert('You Lose you dummy');
-    //         scoreBoard.cpuScore++;
-    //     } 
-    //     alert(`the score is Player: ${scoreBoard.playerScore} to CPU: ${scoreBoard.cpuScore}`);
-    //     scoreBoard.winnerCheck();
-    //     alert('Cpu is choosing new play');
-    //     cpuChoice = choices[Math.floor(Math.random() * choices.length)];
-    // }
+    function playGame() {
+        var playerQuery = magicPlayersRef.where('players');
+        if (playerQuery != 'Jordan' || 'Dillon' || 'Blake' || 'Tone' || 'Will') {
+            magicPlayers.set({
+                Jordan: {
+                    score: 0,
+                },
+        
+                Dillon: {
+                    score: 0,
+                },
+        
+                Tone: {
+                    score: 0,
+                },
+        
+                Blake: {
+                    score: 0,
+                },
+        
+                Will: {
+                    score: 0,
+                },
+            });
+        } else {
+            return usersChoice
+        }   
+    }
 });
