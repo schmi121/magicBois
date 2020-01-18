77e9cc934d3b90168bc0bc3cc13f5ecd8be17c41
Jordan Schmidt <57489956+schmi121@users.noreply.github.com>
2020-01-17 16:18:03 -0800

    backend connected

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+    const players = {
+        Jordan: {
+            score: 0,    
+        },
+        Dillon: {
+            score: 0,    
+        },
+        Blake: {
+            score: 0,    
+        },
+        Tone: {
+            score: 0,    
+        },
+        Will: {
+            score: 0,    
+        }
+    }
+
     var firebaseConfig = {
         apiKey: "AIzaSyAkTJiWbAIVZtkmYlLCbOV5rdgNBU6qkZo",
         authDomain: "magicbois.firebaseapp.com",
@@ -13,11 +32,18 @@ document.addEventListener('DOMContentLoaded', function () {
     var firebaseInstance = firebase.initializeApp(firebaseConfig);
     console.log(firebaseInstance)
 
-    var magicPlayers = firebase.database().ref("/magic/players");
+    var databaseReference = firebase.database();
+    function initializeDatabase() {
+        databaseReference.ref('/magic/players').on("value", function (snapshot) {
+            console.log(snapshot.val());
+            if (!snapshot.val()) {
+                console.log("There's no data")
+                databaseReference.ref('/magic/players').set (players)
+            }
+        });
+    }
 
-    let usersChoice;
-
-    let currentScoreRef = firebase.database().ref('magic/players/').child('/score');
+    initializeDatabase();
 
     const jordanButton = document.getElementById('jordan');
     const blakeButton = document.getElementById('blake');
@@ -25,100 +51,32 @@ document.addEventListener('DOMContentLoaded', function () {
     const dillonButton = document.getElementById('dillon');
     const willButton = document.getElementById('will');
 
-    jordanButton.addEventListener('click', () => {
-        usersChoice = 'jordan';
-        playGame();
+    jordanButton.addEventListener('click', (e) => {
+        incrementScoreByOne(e.srcElement.innerText);
     });
 
-    blakeButton.addEventListener('click', () => {
-        usersChoice = 'blake';
-        playGame();
+    blakeButton.addEventListener('click', (e) => {
+        incrementScoreByOne(e.srcElement.innerText);
     });
 
-    toneButton.addEventListener('click', () => {
-        usersChoice = 'tone';
-        playGame();
+    toneButton.addEventListener('click', (e) => {
+        incrementScoreByOne(e.srcElement.innerText);
     });
 
-    dillonButton.addEventListener('click', () => {
-        usersChoice = 'dillon';
-        playGame();
-    });
-    willButton.addEventListener('click', () => {
-        usersChoice = 'will';
-        playGame();
+    dillonButton.addEventListener('click', (e) => {
+        incrementScoreByOne(e.srcElement.innerText);
     });
 
-    var magicPlayersRef = firebase.database().ref();
-
-    magicPlayersRef.on("value", function (snapshot) {
-        console.log(snapshot.val());
-    }, function (error) {
-        console.log("Error: " + error.code);
+    willButton.addEventListener('click', (e) => {
+        incrementScoreByOne(e.srcElement.innerText);
     });
 
-    function playGame() {
-        var playerQuery = magicPlayersRef;
-        if (playerQuery != 'Jordan' || 'Dillon' || 'Blake' || 'Tone' || 'Will') {
-            magicPlayers.set({
-                Jordan: {
-                    score: 0,
-                },
-
-                Dillon: {
-                    score: 0,
-                },
-
-                Tone: {
-                    score: 0,
-                },
-
-                Blake: {
-                    score: 0,
-                },
-
-                Will: {
-                    score: 0,
-                },
-            });
-        } else function score() {
-            if (usersChoice === 'jordan') {
-                function increaseScore() {
-                    var jordanScoreRef = firebase.database().ref('magic/players/Jordan').child('score');
-                    jordanScoreRef.transaction(function (currentScoreRef) {
-                        return currentScore + 1;
-                    });
-                };
-            } else if (usersChoice === 'blake') {
-                function increaseScore() {
-                    var blakeScoreRef = firebase.database().ref('magic/players/Blake').child('score');
-                    blakeScoreRef.transaction(function (currentScoreRef) {
-                        return currentScore + 1;
-                    });
-                };
-            } else if (usersChoice === 'tone') {
-                function increaseScore() {
-                    var toneScoreRef = firebase.database().ref('magic/players/Tone').child('score');
-                    toneScoreRef.transaction(function (currentScoreRef) {
-                        return currentScore + 1;
-                    });
-                };
-            } else if (usersChoice === 'dillon') {
-                function increaseScore() {
-                    var dillonScoreRef = firebase.database().ref('magic/players/Dillon').child('score');
-                    dillonScoreRef.transaction(function (currentScoreRef) {
-                        return currentScore + 1;
-                    });
-                };
-            } else if (usersChoice === 'will') {
-                function increaseScore() {
-                    var willScoreRef = firebase.database().ref('magic/players/Will').child('score');
-                    willScoreRef.transaction(function (currentScoreRef) {
-                        return currentScore + 1;
-                    });
-                };
-            } else (console.log('Null Pick')); {
-            };
-        };
+    function incrementScoreByOne(player) {
+        databaseReference.ref(`magic/players/${player}`).child('score').transaction(function (currentScore){
+            return currentScore + 1;
+        });
     };
-})
+
+
+    
+})  
---
+++ util.js
@@ -0,0 +1,21 @@
+
+const players = {
+    Jordan: {
+        score: 0,    
+    },
+    Dillon: {
+        score: 0,    
+    },
+    Blake: {
+        score: 0,    
+    },
+    Tone: {
+        score: 0,    
+    },
+    Will: {
+        score: 0,    
+    }
+}
+
+
+module.exports = players;
